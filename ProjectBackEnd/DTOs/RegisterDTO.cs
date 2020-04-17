e40673e5254b4cc017702602a2b56baeb2a02288
Jonas Haenebalcke <jonas.haenebalcke@student.hogent.be>
2020-04-17 16:54:02 +0200

    Bugfix naamgeving

--- ProjectBackEnd/DTOs/RegisterDTO.cs
+++ ProjectBackEnd/DTOs/RegisterDTO.cs
@@ -18,6 +18,6 @@ namespace ProjectBackEnd.DTOs
         [Compare("Wachtwoord")]
         //[RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         [StringLength(50, MinimumLength =6)]    
-        public String WachwoordBevestiging { get; set; }
+        public String BevestigWachtwoord { get; set; }
     }
 }
